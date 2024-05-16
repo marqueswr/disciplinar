3613f283dd0df6d5d199dd71ca8827c2e025b1d7
betowrm <marqueswr@outlook.com>
2024-05-15 20:55:16 -0400

    alterada a rota

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//rotas de autenticação do usuário pelo breeze
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
